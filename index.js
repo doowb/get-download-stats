f7598d577e3ccf9bc58d5ade93da2a8c3729d52e
Brian Woodward <brian.woodward@gmail.com>
2016-01-19 20:24:40 -0500

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * get-download-stats <https://github.com/doowb/get-download-stats>
+ *
+ * Copyright (c) 2016, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
